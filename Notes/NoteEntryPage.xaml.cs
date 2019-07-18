905ff236bede3be80f449b3022006ea2cabf6093
Ian Bret Anthony Bombales <ibombales24@gmail.com>
2019-07-18 21:03:55 +0800

    Master branch - initial commit

---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked (object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+
+            //if (string.IsNullOrEmpty(note.Filename))
+            //{
+            //    // Save
+            //    var fileName = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+            //    File.WriteAllText(fileName, note.Text);
+            //}
+            //else
+            //{
+            //    // Update
+            //    File.WriteAllText(note.Filename, note.Text);
+            //}
+            
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked (object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+
+            //if (File.Exists(note.Filename))
+            //{
+            //    File.Delete(note.Filename);
+            //}
+
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Notes/NotesPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetNoteAsync();
+            //var notes = new List<Note>();
+
+            //var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            //foreach(var fileName in files)
+            //{
+            //    notes.Add(new Note
+            //    {
+            //        Filename = fileName,
+            //        Text = File.ReadAllText(fileName),
+            //        Date = File.GetCreationTime(fileName)
+            //    });
+            //}
+
+            //listView.ItemsSource = notes
+            //    .OrderBy(d => d.Date)
+            //    .ToList();
+        }
+
+        async void OnNoteAddClicked (object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        async void OnListViewItemSelected (object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
